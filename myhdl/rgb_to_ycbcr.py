6e99de59860c74891b209a2339107084f131db8a
Sangeetha G S <sanggs@users.noreply.github.com>
2017-03-27 07:03:11 +0530

    accelerated rgb_to_ycbcr.py

--- myhdl/rgb_to_ycbcr.py
+++ myhdl/rgb_to_ycbcr.py
@@ -8,9 +8,12 @@ def rgb_to_ycbcr(i_r, i_g, i_g, i_clk, o_r, o_g, o_b):
 
 	@always(i_clk.posedge)
 	def run():
-		red.next =  0.257*r + 0.504*g + 0.098*b + 16
-		gre.next = -0.148*r - 0.291*g + 0.439*b + 128
-		blu.next =  0.439*r - 0.368*g - 0.071*b + 128
+		red.next = (r + r<<1 + r<<2 + r<<8 + g<<9 + g<<2 + b<<6 + b<<5 + b<<2)>>10 + 16
+		#red.next =  0.257*r + 0.504*g + 0.098*b + 16
+		gre.next = (-(r<<7 + r<<4 + r<<2 + r<<1 + r) - (g<<8 + g<<5 + g<<3 + g<<1) + b<<8 + b<<7 + b<<6 + b<<1)>>10 + 128
+		#gre.next = -0.148*r - 0.291*g + 0.439*b + 128
+		blu.next = (r<<8 + r<<7 + r<<6 + r<<1 - (g<<8 + g<<6 + g<<5 + g<<4 + g<<3 + g) - (b<<6 + b<<3 + b))>>10 + 128
+		#blu.next =  0.439*r - 0.368*g - 0.071*b + 128
 
 	@always_comb
 	def outputs():
